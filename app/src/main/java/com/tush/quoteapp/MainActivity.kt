d2f165523d65e285cfe50239398d35a995e9b507
Tushar69k <tushlappy@gmail.com>
2025-11-22 17:10:55 +0530

    The Journey Begins

--- app/src/main/java/com/tush/quoteapp/MainActivity.kt
+++ app/src/main/java/com/tush/quoteapp/MainActivity.kt
@@ -6,8 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
